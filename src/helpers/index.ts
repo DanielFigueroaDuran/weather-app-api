7cec7356e6bfe0bf190bda4920b007689ec5661f
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-09-15 18:20:26 +0200

    Typing API Results - Type Guards

---
+++ src/helpers/index.ts
@@ -0,0 +1,14 @@
+import { Weather } from "../types"
+
+// esta function no es recomendable ya que no es mantenible
+
+export const isWeatherResponse = (weather: unknown): weather is Weather => {
+      return (
+            Boolean(weather) &&
+            typeof weather === 'object' &&
+            typeof (weather as Weather).name === 'string' &&
+            typeof (weather as Weather).main.temp === 'number' &&
+            typeof (weather as Weather).main.temp_max === 'number' &&
+            typeof (weather as Weather).main.temp_min === 'number'
+      )
+}
--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 import { SearchType } from "../types";
+import { isWeatherResponse } from "../helpers";
+
+
+
 
 const useWeather = () => {
 
@@ -12,7 +16,32 @@ const useWeather = () => {
 
                   const { data } = await axios(geoUrl);
 
-                  console.log(data);
+                  const lat = data[0].lat;
+                  const lon = data[0].lon;
+
+                  // console.log(lat);
+                  // console.log(lon);
+
+                  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+                  //console.log(weatherUrl);
+
+                  // ---------Castear el type------------
+
+                  // const { data: weatherResult } = await axios<Weather>(weatherUrl);
+                  // console.log(weatherResult.main.temp);
+
+                  // ---------type Guards------------
+
+                  const { data: weatherResult } = await axios(weatherUrl);
+                  const result = isWeatherResponse(weatherResult);
+
+                  //console.log(result);
+
+                  if (result) {
+                        console.log(weatherResult.name);
+                  } else {
+                        console.log('respuesta mal formada');
+                  }
 
             } catch (error) {
                   console.log(error);
