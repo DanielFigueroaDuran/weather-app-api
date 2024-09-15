dad2a4bce85aaa784977d92206fe601689987f1c
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-09-15 20:43:34 +0200

    Type Results from an API - Validate with Valibot

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,21 +1,35 @@
 import axios from "axios";
-import { z } from "zod";
+//import { z } from "zod";
+import { object, string, number, InferOutput, parse } from "valibot";
 import { SearchType } from "../types";
 import { isWeatherResponse } from "../helpers";
 
-//zod
+//--------zod-----------
 
-const Weather = z.object({
-      name: z.string(),
-      main: z.object({
-            temp: z.number(),
-            temp_max: z.number(),
-            temp_min: z.number()
+// const Weather = z.object({
+//       name: z.string(),
+//       main: z.object({
+//             temp: z.number(),
+//             temp_max: z.number(),
+//             temp_min: z.number()
+//       })
+// });
+
+// type Weather = z.infer<typeof Weather>
+
+
+// ---------Volibot------------
+
+const WeatherSchena = object({
+      name: string(),
+      main: object({
+            temp: number(),
+            temp_max: number(),
+            temp_min: number()
       })
-});
+})
 
-type Weather = z.infer<typeof Weather>
-
+type Weather = InferOutput<typeof WeatherSchena>
 
 const useWeather = () => {
 
@@ -56,14 +70,26 @@ const useWeather = () => {
                   // }
 
                   // -------------Zod----------------
-                  const { data: weatherResult } = await axios(weatherUrl);
-                  const result = Weather.safeParse(weatherResult)
+
+                  // const { data: weatherResult } = await axios(weatherUrl);
+                  // const result = Weather.safeParse(weatherResult)
+                  // //console.log(result);
+
+                  // if (result.success) {
+                  //       console.log(result.data.name);
+                  //       console.log(result.data.main.temp);
+
+                  // }
+
+                  // -------------Valibot----------------
+
+                  const { data: weatherResult } = await axios<Weather>(weatherUrl);
+                  const result = parse(WeatherSchena, weatherResult);
                   //console.log(result);
-
-                  if (result.success) {
-                        console.log(result.data.name);
-                        console.log(result.data.main.temp);
-
+                  if (result) {
+                        console.log(result.name);
+                  } else {
+                        console.log('Respuesta mal formada...');
                   }
 
             } catch (error) {
