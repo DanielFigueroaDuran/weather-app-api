cf2d66d7e7e84bd7b96da8c7c0db29596e54302d
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-09-15 20:11:16 +0200

    Type Results from an API - Validate with ZOD

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,8 +1,20 @@
 import axios from "axios";
+import { z } from "zod";
 import { SearchType } from "../types";
 import { isWeatherResponse } from "../helpers";
 
+//zod
 
+const Weather = z.object({
+      name: z.string(),
+      main: z.object({
+            temp: z.number(),
+            temp_max: z.number(),
+            temp_min: z.number()
+      })
+});
+
+type Weather = z.infer<typeof Weather>
 
 
 const useWeather = () => {
@@ -32,15 +44,26 @@ const useWeather = () => {
 
                   // ---------type Guards------------
 
-                  const { data: weatherResult } = await axios(weatherUrl);
-                  const result = isWeatherResponse(weatherResult);
+                  // const { data: weatherResult } = await axios(weatherUrl);
+                  // const result = isWeatherResponse(weatherResult);
 
                   //console.log(result);
 
-                  if (result) {
-                        console.log(weatherResult.name);
-                  } else {
-                        console.log('respuesta mal formada');
+                  // if (result) {
+                  //       console.log(weatherResult.name);
+                  // } else {
+                  //       console.log('respuesta incorrecta');
+                  // }
+
+                  // -------------Zod----------------
+                  const { data: weatherResult } = await axios(weatherUrl);
+                  const result = Weather.safeParse(weatherResult)
+                  //console.log(result);
+
+                  if (result.success) {
+                        console.log(result.data.name);
+                        console.log(result.data.main.temp);
+
                   }
 
             } catch (error) {
